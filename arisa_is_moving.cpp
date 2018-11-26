c660b80ebc13d128cb7345edacd7a68af426de31
MintChoco <miraak@naver.com>
2018-11-26 13:17:43 +0900

    arisa is moving. just for fun

---
+++ arisa_is_moving.cpp
@@ -0,0 +1,300 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <SDL2_image/SDL_image.h>
+using namespace std;
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+bool init();
+bool loadMedia();
+void close();
+SDL_Surface* loadSurface(string path);
+SDL_Texture* loadTexture(string path);
+
+SDL_Window *gWindow;
+SDL_Surface *gScreenSurface;
+SDL_Surface *gHelloWorld;
+SDL_Renderer *gRenderer;
+SDL_Texture *gTexture;
+
+class LTexture
+{
+public:
+    LTexture();
+    
+    ~LTexture();
+    
+    bool loadFromFile(string path);
+    void free();
+    void render(int x, int y);
+    void move();
+    
+    int getWidth();
+    int getHeight();
+    
+    int pos_x;
+    int pos_y;
+private:
+    SDL_Texture* mTexture;
+    int mWidth;
+    int mHeight;
+};
+
+LTexture::LTexture()
+{
+    mTexture = NULL;
+    mWidth = 0;
+    mHeight = 0;
+    pos_x = 0;
+    pos_y = 0;
+}
+
+LTexture::~LTexture()
+{
+    free();
+}
+
+bool LTexture::loadFromFile(string path)
+{
+    free();
+    SDL_Texture *newTexture = NULL;
+    SDL_Surface *loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL)
+    {
+        cout << SDL_GetError() << endl;
+    }
+    else
+    {
+        SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xff, 0xff));
+        newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        if(newTexture == NULL)
+        {
+            cout << SDL_GetError() << endl;
+        }
+        else
+        {
+            mWidth = loadedSurface->w;
+            mHeight = loadedSurface->h;
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    mTexture = newTexture;
+    return mTexture != NULL;
+}
+
+void LTexture::free()
+{
+    if(mTexture != NULL)
+    {
+        SDL_DestroyTexture(mTexture);
+        mTexture = NULL;
+        mWidth = 0;
+        mHeight = 0;
+        pos_x = 0;
+        pos_y = 0;
+    }
+}
+
+void LTexture::render(int x, int y)
+{
+    SDL_Rect renderQuad = {x, y, mWidth, mHeight};
+    SDL_RenderCopy(gRenderer, mTexture, NULL, &renderQuad);
+}
+
+void LTexture::move()
+{
+    SDL_Rect renderQuad = {pos_x, pos_y, mWidth, mHeight};
+    SDL_RenderCopy(gRenderer, mTexture, NULL, &renderQuad);
+}
+
+int LTexture::getWidth()
+{
+    return mWidth;
+}
+
+int LTexture::getHeight()
+{
+    return mHeight;
+}
+
+LTexture gFooTexture;
+LTexture gBackgroundTexture;
+
+int main(int argc, const char * argv[])
+{
+    if(!init())
+    {
+        cout << "Error in init()" << endl; exit(1);
+    }
+    if(!loadMedia())
+    {
+        cout << "Error in loadMedia()" << endl; exit(1);
+    }
+    
+    bool quit = false;
+    SDL_Event e;
+    
+    while(!quit)
+    {
+        while(SDL_PollEvent(&e) != 0)
+        {
+            if(e.type == SDL_QUIT)
+            {
+                quit = true;
+            }
+            if(e.type == SDL_KEYDOWN)
+            {
+                switch (e.key.keysym.sym) {
+                    case SDLK_UP:
+                        gFooTexture.pos_y -= 5;
+                        break;
+                    case SDLK_DOWN:
+                        gFooTexture.pos_y += 5;
+                        break;
+                    case SDLK_RIGHT:
+                        gFooTexture.pos_x += 5;
+                        break;
+                    case SDLK_LEFT:
+                        gFooTexture.pos_x -= 5;
+                        break;
+                        
+                    default:
+                        break;
+                }
+            }
+        }
+        
+        SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+        SDL_RenderClear(gRenderer);
+        
+        gBackgroundTexture.render(0, 0);
+  //      gFooTexture.render(240, 190);
+        gFooTexture.move();
+        
+        SDL_RenderPresent(gRenderer);
+    }
+    close();
+    return 0;
+}
+
+bool init()
+{
+    bool ret = true;
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        cout << SDL_GetError() << endl;
+        ret = false;
+    }
+    else
+    {
+        gWindow = SDL_CreateWindow("Hello, SDL!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(gWindow == NULL)
+        {
+            cout << SDL_GetError() << endl;
+            ret = false;
+        }
+        else
+        {
+            gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+            if(gRenderer == NULL)
+            {
+                cout << SDL_GetError() << endl;
+                ret = false;
+            }
+            else
+            {
+                SDL_SetRenderDrawColor(gRenderer, 0xff, 0xff, 0xff, 0xff);
+                
+                int imgFlags = IMG_INIT_PNG;
+                if(!(IMG_Init(imgFlags) & imgFlags))
+                {
+                    cout << SDL_GetError() << endl;
+                    ret = false;
+                }
+            }
+        }
+    }
+    
+    return ret;
+}
+
+bool loadMedia()
+{
+    bool ret = true;
+    if(!gFooTexture.loadFromFile("/arisa.jpg"))
+    {
+        cout << SDL_GetError() << endl;
+        ret = false;
+    }
+    
+    if(!gBackgroundTexture.loadFromFile("/arisa.jpg"))
+    {
+        cout << SDL_GetError() << endl;
+        ret = false;
+    }
+    
+    return ret;
+}
+
+void close()
+{
+    gFooTexture.free();
+    gBackgroundTexture.free();
+    
+    SDL_DestroyRenderer(gRenderer);
+    gRenderer = NULL;
+    
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+    
+    IMG_Quit();
+    SDL_Quit();
+}
+
+SDL_Surface* loadSurface(string path)
+{
+    SDL_Surface* ret = NULL;
+    
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL)
+    {
+        cout << SDL_GetError() << endl;
+    }
+    else
+    {
+        ret = SDL_ConvertSurface(loadedSurface, gScreenSurface->format, NULL);
+        if(ret == NULL)
+        {
+            cout << SDL_GetError() << endl;
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    
+    return ret;
+}
+
+SDL_Texture* loadTexture(string path)
+{
+    SDL_Texture* ret = NULL;
+    SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+    if(loadedSurface == NULL)
+    {
+        cout << SDL_GetError() << endl;
+    }
+    else
+    {
+        ret = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+        if(ret == NULL)
+        {
+            cout << SDL_GetError() << endl;
+        }
+        SDL_FreeSurface(loadedSurface);
+    }
+    
+    
+    return ret;
+}
